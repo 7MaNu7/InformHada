5f10c5d7d3162ee9b9a09e9f55e905bc80fc6dc6
7MaNu7 <manugustalol@gmail.com>
2014-05-16 18:29:17 +0200

    ComentarioEN y ComentarioCAD a falta de comentar
    Ahora los comento

---
+++ Film/FilmBiblio/ComentarioCAD.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Configuration;
+
+namespace FilmBiblio
+{
+    class ComentarioCAD
+    {
+        ///////////
+        // Datos //
+        ///////////
+
+        private string conexion = ConfigurationManager.ConnectionStrings["ApplicationServices"].ToString();
+
+        ///////////////
+        // Funciones //
+        ///////////////
+
+        public void InsertarComentario(ComentarioEN comentario)
+        {
+            String orden = "insert into comentario values ";
+            orden += "( " + comentario.Id + ", ";
+            orden += "'" + comentario.Texto + "', ";
+            orden += comentario.Usuario + ", ";
+            orden += "'" + comentario.Fecha + "', ";
+            orden += comentario.Film + ")";
+
+            SqlConnection c = null;
+            try
+            {
+                c = new SqlConnection(conexion);
+                c.Open();
+                SqlCommand insert_comentario = new SqlCommand(orden, c);
+                insert_comentario.ExecuteNonQuery();
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            finally { c.Close(); }
+        }
+
+        public void UpdateComentario(ComentarioEN comentario)
+        {
+            String orden = "update comentario ";
+            orden += "set texto = '" + comentario.Texto + "', ";
+            orden += "usuario = " + comentario.Usuario + ", ";
+            orden += "fecha = '" + comentario.Fecha + "', ";
+            orden += "film = " + comentario.Film + ", ";
+            orden += "where id = " + comentario.Id;
+
+            SqlConnection c = null;
+            try
+            {
+                c = new SqlConnection(conexion);
+                c.Open();
+                SqlCommand update_comentario = new SqlCommand(orden, c);
+                update_comentario.ExecuteNonQuery();
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            finally { c.Close(); }
+        }
+
+        //Borra una película en la BD que tiene la clave primaria que se pasa por parámetro
+        public void BorrarPelicula(int id)
+        {
+            String orden = "delete from comentario where id= " + id;
+
+            SqlConnection c = null;
+            try
+            {
+                c = new SqlConnection(conexion);
+                c.Open();
+                SqlCommand delete_comentario = new SqlCommand(orden, c);
+                delete_comentario.ExecuteNonQuery();
+
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            finally { c.Close(); }
+        }
+
+        //Devuelve la información de todas las películas
+        public DataSet DameComentarios()
+        {
+            SqlConnection c = new SqlConnection(conexion);
+            DataSet bdvirtual = new DataSet();
+
+            try
+            {
+                String select_comentarios = "Select * from comentario";
+                SqlDataAdapter ejecuta = new SqlDataAdapter(select_comentarios, c);
+                ejecuta.Fill(bdvirtual, "comentarios");
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            finally { c.Close(); }
+
+            return bdvirtual;
+        }
+
+        //Devuelve la información de la película que tiene como clave primaria el id pasado por parámetro
+        public ComentarioEN DameComentario(int id)
+        {
+            SqlConnection c = null;
+            ComentarioEN comentario = null;
+
+            try
+            {
+                c = new SqlConnection(conexion);
+                c.Open();
+                SqlCommand select_comentario = new SqlCommand("Select * from comentario where id=" + id, c);
+                SqlDataReader read = select_comentario.ExecuteReader();
+
+                comentario = new ComentarioEN((int)read["id"], (string)read["texto"], (int)read["usuario"],
+                    (string)read["fecha"], (int)read["film"]);
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            finally { c.Close(); }
+
+            return comentario;
+        }
+    }
+}
---
+++ Film/FilmBiblio/ComentarioEN.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+namespace FilmBiblio
+{
+    class ComentarioEN
+    {
+        ///////////
+        // Datos //
+        ///////////
+
+        private ComentarioCAD comentarioCad = new ComentarioCAD();//Instancia de PeliculaCAD para gestionar la información de usuarios en la base de datos
+        private DataSet bd = new DataSet();
+
+        private int id;                      //identificador del comentario
+        private string texto;                //contenido del comentario
+        private int usuario;                 //identificador para el usuario
+        private string fecha;                //fecha en que se realiza el comentario
+        private int film;                    //identificador para la serie o pelicula
+
+        ///////////////
+        // Funciones //
+        ///////////////
+
+        //Constructor por defecto
+        public ComentarioEN() { }
+
+                //Constructor con parámetros que son las propiedades de dicho capítulo
+        public ComentarioEN(int pid, string ptexto, int pusuario, string pfecha, int pfilm)
+        {
+            id = pid;
+            texto = ptexto;
+            usuario = pusuario;
+            fecha = pfecha;
+            film = pfilm;
+        }
+
+        //Se inserta en la BD el nuevo capítulo cuyos datos están en esta instancia this de CapituloEN
+        public void InsertarComentario()
+        {
+            comentarioCad.InsertarComentario(this);
+        }
+
+        //Se modifica en la BD un capítulo cuyos datos están en esta instancia this de CapituloEN
+        public void UpdateComentario()
+        {
+            comentarioCad.UpdateComentario(this);
+        }
+
+        //Se borra en la BD un capítulo diferenciado de los demás por su id
+        public void BorrarComentario()
+        {
+            comentarioCad.BorrarComentario(this.id);
+        }
+
+        //Devuelve la información de todas las películas
+        public DataSet DameComentarios()
+        {
+            try
+            {
+                bd = comentarioCad.DameComentarios();
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            return bd;
+        }
+
+        //Devuelve la información de la película que tiene como clave primaria el id pasado por parámetro
+        public ComentarioEN DameComentario()
+        {
+            ComentarioEN comentario = new ComentarioEN();
+            comentario = comentarioCad.DameComentario(this.id);
+            return comentario;
+        }
+
+        /////////////////
+        // Propiedades //
+        /////////////////
+
+        //Desde fuera de la clase se puede obtener el id y modificarlo
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        //Desde fuera de la clase se puede obtener el id y modificarlo
+        public string Texto
+        {
+            get { return texto; }
+            set { texto = value; }
+        }
+
+        //Desde fuera de la clase se puede obtener el id y modificarlo
+        public int Usuario
+        {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+
+        //Desde fuera de la clase se puede obtener el id y modificarlo
+        public string Fecha
+        {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+
+        //Desde fuera de la clase se puede obtener el id y modificarlo
+        public int Film
+        {
+            get { return film; }
+            set { film = value; }
+        }
+    }
+}
