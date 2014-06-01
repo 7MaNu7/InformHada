da97342c1e931c8ad29aa1f509d010fadd9c6fc7
EncarnaAmoros <encaramorosb@gmail.com>
2014-06-01 08:06:55 +0200

    Comentarios email +si "" no envía y mensaje error

--- Film/WebApplication1/Report.aspx.cs
+++ Film/WebApplication1/Report.aspx.cs
@@ -21,9 +21,22 @@ namespace WebApplication1
 
         }
 
+        //Para enviar el email de la queja, consulta o sugerencia
         protected void EnviarEmail(object sender, EventArgs e)
         {
+            //Texto a enviar por el usuario
+            String texto_report = TextBoxReport.Text;
+
+            //Si no se ha escrito ningún texto, mensaje indicándolo y no se envia el email
+            if (texto_report == "")
+            {
+                LabelConfirmacion.Text = "¡El cuerpo del mensaje está vacío, escriba su queja, consulta o sugerencia y pulse en Enviar.";
+                return;
+            }
+
+            //Datos del usuario si está registrado
             usuario = (FilmBiblio.UsuarioEN)Session["usuario"];
+            //Servidor, en este caso gmail con el puerto 587
             SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
             MailMessage mensaje = new MailMessage();
 
@@ -33,22 +46,26 @@ namespace WebApplication1
 
                 if (usuario != null)
                 {
+                    //Si está registrado aparecerá como email de envio el del usuario
                     de_quien = new MailAddress(usuario.Email, usuario.Usuario);
                 }
                 else
                 {
+                    //Si no está registrado, es desconocido
                     de_quien = new MailAddress("desconocido@gmail.com", "desconocido");
                 }
 
+                //Se envia al correo oficial de la aplicación
                 MailAddress a_quien = new MailAddress("informhada@gmail.com", "InformaTV");
-                mensaje.From = de_quien;
 
-                mensaje.To.Add(a_quien);
-                mensaje.Subject = "Report sobre InformaTV";
-                String texto_report = TextBoxReport.Text;
-                mensaje.Body = texto_report;
-                smtpClient.EnableSsl = true;
 
+                mensaje.From = de_quien;//Solo viene de una persona
+                mensaje.To.Add(a_quien);//Solo va destinado al correo oficial de la aplicación
+                mensaje.Subject = "Report sobre InformaTV";//El asunto
+                mensaje.Body = texto_report;//El texto del correo es lo que el usuario ha escrito en el textbox
+                smtpClient.EnableSsl = true;//Para mayor seguridad
+
+                //Un email y su contraseña desde los cuales se envia realmente el correo (aunque luego aparezca  que es el usuario)
                 smtpClient.Credentials = new System.Net.NetworkCredential("informhada@gmail.com", "eduardomanteca");
                 smtpClient.Send(mensaje);
 
@@ -57,6 +74,7 @@ namespace WebApplication1
             }
             catch (Exception ex)
             {
+                //Si hubo errores al enviar
                 Console.WriteLine(ex.Message);
                 LabelConfirmacion.Text = "No se pudo enviar el mensaje. Inténtelo de nuevo en unos minutos";  
             }
